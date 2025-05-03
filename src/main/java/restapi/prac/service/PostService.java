8830b3acba4e96b7470529659b277d3b29a43c18
엄경문 <eomkyeongmun@naver.com>
2025-05-03 16:10:51 +0900

    Upload

---
+++ src/main/java/restapi/prac/service/PostService.java
@@ -0,0 +1,39 @@
+package restapi.prac.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import restapi.prac.moodel.Post;
+import restapi.prac.repository.PostRepository;
+
+import java.util.Optional;
+
+@Service
+public class PostService {
+    @Autowired
+    private PostRepository postRepository;
+
+    public Page<Post> getPosts(Pageable pageable) {
+        return postRepository.findAll(pageable);
+    }
+    public Optional<Post> getPost(Long id) {
+        return postRepository.findById(id);
+    }
+    public Post createPost(Post post) {
+        return postRepository.save(post);
+    }
+    public Optional<Post> updatePost(Long id, Post updatePost) {
+        return postRepository.findById(id).map(post->{
+            post.setTitle(updatePost.getTitle());
+            post.setContent(updatePost.getContent());
+            return postRepository.save(post);
+        });
+    }
+    public boolean deletePost(Long id) {
+        return postRepository.findById(id).map(post->{
+            postRepository.delete(post);
+            return true;
+        }).orElse(false);
+    }
+}
